c8c8556f05734e50143ad4ff5bb5b8cf412959d3
nnadya <n.d.niki2012@gmail.com>
2026-02-19 22:34:08 +0300

    add fetchUsers

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,1 +1,23 @@
-// fetch-запрос на получение данных
+const BASE_URL = 'https://dummyjson.com/users';
+
+export const fetchUsers = async (limit = 30, skip = 0) => {
+  try {
+    const response = await fetch(`${BASE_URL}?limit=${limit}&skip=${skip}`);
+    
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    // Проверяем, что есть поле users
+    if (!data.users) {
+      throw new Error('Неверный формат ответа от сервера');
+    }
+
+    return data.users;
+  } catch (error) {
+    console.error('Ошибка при загрузке пользователей:', error.message);
+    throw error; // пробрасываем дальше, чтобы компонент мог обработать
+  }
+};
