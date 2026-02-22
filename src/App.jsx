9cf62458a0bd84052664842c97cd5b8e3e880db1
nnadya <n.d.niki2012@gmail.com>
2026-02-22 18:24:07 +0300

    add 'SortingControls' and update 'Table'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState, useEffect } from 'react'
-import { fetchUsers } from './api/usersApi'
+import { useState } from 'react';
 
-import { Table } from './components/Table'
+import { Table } from './components/Table';
+import { SortingControls } from './components/SortingControls';
 //import './App.css'
 
 function App() {
+  const [sortField, setSortField] = useState('');
+  const [sortOrder, setSortOrder] = useState('none');
+
+/*
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
@@ -26,12 +30,21 @@ function App() {
   }, []);
 
   if (loading) return <div style={{ textAlign: 'center', marginTop: '50px' }}>Загрузка пользователей...</div>;
-  if (error) return <div style={{ textAlign: 'center', marginTop: '50px', color: 'red' }}>Ошибка: {error}</div>;
+  if (error) return <div style={{ textAlign: 'center', marginTop: '50px', color: 'red' }}>Ошибка: {error}</div>; */
 
   return (
     <div style={{ padding: '20px' }}>
       <h1 style={{ textAlign: 'center' }}>Список пользователей</h1>
-      <Table users={users} />
+      <SortingControls
+        sortField={sortField}
+        sortOrder={sortOrder}
+        onSortFieldChange={setSortField}
+        onSortOrderChange={setSortOrder}
+      />
+      <Table
+        sortField={sortField}
+        sortOrder={sortOrder}
+      />
     </div>
   );
 };
