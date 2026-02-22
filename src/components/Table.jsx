838ea002694474e1a80cfd08d7e9c7a29d31d134
nnadya <n.d.niki2012@gmail.com>
2026-02-22 20:00:59 +0300

    add pagination and onTotalChange

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,10 +4,7 @@ import { fetchUsers } from "../api/usersApi";
 import { Header } from "./Header";
 import { User } from "./User";
 
-export const Table = ({ sortField, sortOrder }) => {
-/*   const [sortField, setSortField] = useState('');
-  const [sortOrder, setSortOrder] = useState(''); */
-
+export const Table = ({ sortField, sortOrder, currentPage, limit, onTotalChange }) => {
   const [usersData, setUsersData] = useState([]);
 
   useEffect(() => {
@@ -15,26 +12,28 @@ export const Table = ({ sortField, sortOrder }) => {
       try {
         const params = {};
 
-/*      if (sortField) params.sortBy = sortField;
-        if (sortOrder && sortOrder !== 'none') params.order = sortOrder; */
-
         // Сортировка применяется только если выбрано и поле, и порядок
         if (sortField && sortOrder !== 'none') {
           params.sortBy = sortField;
           params.order = sortOrder;
         }
 
+        // пагинация
+        params.limit = limit;
+        params.skip = (currentPage - 1) * limit;
+
         const query = new URLSearchParams(params).toString();
         //const result = await fetchUsers(query ? `${query}` : '');
         const result = await fetchUsers(query);
-        setUsersData(result);
+        setUsersData(result.users);
+        onTotalChange(result.total);
       } catch (error) {
         console.error(error);
       }
     };
 
     loadUsers();
-  }, [sortField, sortOrder]);
+  }, [sortField, sortOrder, currentPage, limit, onTotalChange]);
 
   return (
     <table>
