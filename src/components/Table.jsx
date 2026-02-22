f81fb4cfc0d6d3ec885e21aa9c469701940af76b
nnadya <n.d.niki2012@gmail.com>
2026-02-22 18:20:48 +0300

    update sorting

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,20 +4,29 @@ import { fetchUsers } from "../api/usersApi";
 import { Header } from "./Header";
 import { User } from "./User";
 
-export const Table = () => {
-  const [sortField, setSortField] = useState('');
-  const [sortOrder, setSortOrder] = useState('');
+export const Table = ({ sortField, sortOrder }) => {
+/*   const [sortField, setSortField] = useState('');
+  const [sortOrder, setSortOrder] = useState(''); */
+
   const [usersData, setUsersData] = useState([]);
 
   useEffect(() => {
     const loadUsers = async () => {
       try {
         const params = {};
-        if (sortField) params.sortBy = sortField;
-        if (sortOrder && sortOrder !== 'none') params.order = sortOrder;
+
+/*      if (sortField) params.sortBy = sortField;
+        if (sortOrder && sortOrder !== 'none') params.order = sortOrder; */
+
+        // Сортировка применяется только если выбрано и поле, и порядок
+        if (sortField && sortOrder !== 'none') {
+          params.sortBy = sortField;
+          params.order = sortOrder;
+        }
 
         const query = new URLSearchParams(params).toString();
-        const result = await fetchUsers(query ? `${query}` : '');
+        //const result = await fetchUsers(query ? `${query}` : '');
+        const result = await fetchUsers(query);
         setUsersData(result);
       } catch (error) {
         console.error(error);
@@ -28,31 +37,13 @@ export const Table = () => {
   }, [sortField, sortOrder]);
 
   return (
-    <>
-      <div className="sorting-controls">
-        <select value={sortField} onChange={e => setSortField(e.target.value)}>
-          <option value="">Поле для сортировки</option>
-          <option value="firstName">Имя</option>
-          <option value="lastName">Фамилия</option>
-          <option value="maidenName">Отчество</option>
-          <option value="age">Возраст</option>
-          <option value="gender">Пол</option>
-          <option value="phone">Номер телефона</option>
-        </select>
-        <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
-          <option value="none">Без сортировки</option>
-          <option value="asc">По возрастанию</option>
-          <option value="desc">По убыванию</option>
-        </select>
-      </div>
-      <table>
-        <Header />
-        <tbody>
-          {usersData.map(user => (
-            <User key={user.id} user={user} />
-          ))}
-        </tbody>
-      </table>
-    </>
+    <table>
+      <Header />
+      <tbody>
+        {usersData.map(user => (
+          <User key={user.id} user={user} />
+        ))}
+      </tbody>
+    </table>
   );
 };
